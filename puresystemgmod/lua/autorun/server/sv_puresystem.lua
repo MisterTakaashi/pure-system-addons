ed1b25c1de7fae2b9de2259052431571442b014e
Achild0 <lesbatsvirgile@hotmail.com>
2015-05-31 05:20:00 +0200

    Ajout que admin (mini) pour ouvrir le pure
    Faudrait aussi faire un system que les superadmins puissent ajouter les
    admins au pure ou un truc du style

--- puresystemgmod/lua/autorun/server/sv_puresystem.lua
+++ puresystemgmod/lua/autorun/server/sv_puresystem.lua
@@ -4,7 +4,7 @@ util.AddNetworkString("OpenGuiPure")
 
 hook.Add( "PlayerSay", "OpenPurePanel", function(ply, text, public)
 	text = string.lower(text)
-	if (string.sub(text, 1,5) == "!pure") then
+	if (string.sub(text, 1,5) == "!pure") and ply:IsAdmin() then
 		net.Start("OpenGuiPure")
 		net.Send(ply)
 	end
