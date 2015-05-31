40cce3bc88759bfb912f37cac31760ca86008cef
Achild0 <lesbatsvirgile@hotmail.com>
2015-06-01 01:33:10 +0200

    pti changement de traduction

--- puresystemgmod/lua/autorun/pure_config.lua
+++ puresystemgmod/lua/autorun/pure_config.lua
@@ -1,20 +1,27 @@
 AddCSLuaFile()
-/* Ici vous touchez comme vous le voulez :P,
-c'est un fichier de config où vous pouvez changer les valeurs de rep pour votre serveur par exemple !
-Certaines valeurs se change sur votre page sur le Site : http://puresystem.fr
-*/
+/* Ici vous ne touchez pas encore comme vous le voulez :P */
 
 PURE = {}
 
 
 	PURE.minauthorisedrep = 75
 
-	PURE.maxauthorisedrep = 100  -- Here is the Minimum reputation required to be allowed to join the server
+	PURE.maxauthorisedrep = 100
 
 	PURE.minauthorisatedrprep = 50
 
-	PURE.maxauthorisatedrprep = 100 -- Here is the Minimum ROLEPLAY reputation requiered to join the server
+	PURE.maxauthorisatedrprep = 100
+	--------------------------------------------------------------------OnPeutToucher:D-----------------------------------------------------------
+	/* Ici vous touchez comme vous le voulez :P,
+	c'est un fichier de config où vous pouvez changer les groupes pour le Pure pour votre serveur par exemple !
+	Certaines valeurs se change sur votre page sur le Site : http://puresystem.fr
+	Merci de faire confience au PureSystem
+	Bonne Config et Bisous ! :)
+	*/
 
+	/* Vous pouvez ajouter des groupes pouvant ouvrir le PureSystem !
+		 n'oubliez pas de mettre le nom exacte du groupe entre '' et avec une "," à la fin ! ^_^
+	*/
 	PURE.authgrp = {
 		'superadmin',
 		'admin'
