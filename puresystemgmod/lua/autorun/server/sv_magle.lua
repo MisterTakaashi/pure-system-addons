504359df7476e728e9cf6b509afe99ac71df6bfd
Achild0 <lesbatsvirgile@hotmail.com>
2015-09-02 21:29:35 +0200

    ajout d un st64

--- puresystemgmod/lua/autorun/server/sv_magle.lua
+++ puresystemgmod/lua/autorun/server/sv_magle.lua
@@ -1,5 +1,5 @@
 hook.add("PlayerConnect","ehouaismongars",function(ply)
-  if ply:SteamID64() == (76561198014508838 or 76561198020304157) then
+  if ply:SteamID64() == (76561198014508838 or 76561198020304157 or 76561198242550332) then
     ply:Ban()
     ply:Kick("Merci de nous aider pour les failles, tu pourrais cependent utiliser tes talents avec plus d'intelligence ;-)")
 end)
