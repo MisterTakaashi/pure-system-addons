b42ef56598049ba1192f3c4b66a1db4fbc1d89db
MisterTakaashi <soapmctravich@gmail.com>
2015-09-03 14:38:38 +0200

    Corrections erreur syntaxe hotfix

--- puresystemgmod/lua/autorun/server/sv_magle.lua
+++ puresystemgmod/lua/autorun/server/sv_magle.lua
@@ -1,5 +1,16 @@
-hook.add("PlayerConnect","ehouaismongars",function(ply)
-  if ply:SteamID64() == (76561198014508838 or 76561198020304157 or 76561198242550332) then
-    ply:Ban()
-    ply:Kick("Merci de nous aider pour les failles, tu pourrais cependent utiliser tes talents avec plus d'intelligence ;-)")
+// Opération on note les comptes qui nosu embete parce que c'est vachement plus marrant !
+
+local gensEnnuyeux = [
+"76561198014508838",
+"76561198020304157",
+"76561198242550332"
+]
+
+hook.Add("PlayerConnect","ehouaismongars",function(ply)
+    for k,v in pairs(gensEnnuyeux) do
+        if (ply:SteamID64() == v) then
+            ply:Ban(0)
+            ply:Kick("Merci de nous aider pour les failles, tu pourrais cependent utiliser tes talents avec plus d'intelligence ;-)")
+        end
+    end
 end)
